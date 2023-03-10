c1f903dc9a080a607059ad03d095a5f9c8e13331
jurian112211 <87150143+jurian112211@users.noreply.github.com>
2023-03-10 20:22:31 +0100

    Changes for 2.0.

--- src/main/java/nl/juriantech/disableadchievements/Main.java
+++ src/main/java/nl/juriantech/disableadchievements/Main.java
@@ -1,6 +1,8 @@
 package nl.juriantech.disableadchievements;
 
+import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -9,24 +11,54 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
 
+    private boolean showDisabledAchievementsMessage;
+    private String disabledAchievementsMessage;
+    private String[] ignoredAchievements;
+
     @Override
     public void onEnable() {
-        getLogger().severe("Disable-adchievements is now enabled!");
+        getLogger().severe("Disable-achievements is now enabled!");
         getServer().getPluginManager().registerEvents(this, this);
+
+        // Load configuration
+        saveDefaultConfig();
+        FileConfiguration config = getConfig();
+        showDisabledAchievementsMessage = config.getBoolean("show-disabled-achievements-message", true);
+        disabledAchievementsMessage = config.getString("disabled-achievements-message", "Achievements are currently disabled!");
+        ignoredAchievements = config.getStringList("ignored-achievements").toArray(new String[0]);
     }
 
     @Override
     public void onDisable() {
-        getLogger().severe("Disable-adchievements is now disabled!");
+        getLogger().severe("Disable-achievements is now disabled!");
     }
-    private void hideAdchievements(World world) {
+
+    private boolean isIgnoredAchievement(String achievementName) {
+        for (String ignored : ignoredAchievements) {
+            if (achievementName.equals(ignored)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void hideAchievements(World world) {
         world.setGameRuleValue("announceAdvancements", "false");
         getLogger().info("Achievements are now hidden for the world: " + world.getName() + ".");
     }
 
-    /* Hide advancements for new worlds. */
+    /* Hide achievements for new worlds. */
     @EventHandler(priority = EventPriority.MONITOR)
     public void onWorldLoad(WorldLoadEvent event) {
-        hideAdchievements(event.getWorld());
+        hideAchievements(event.getWorld());
+    }
+
+    /* Show disabled achievements message to player. */
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onAchievementCompleted(PlayerAdvancementDoneEvent event) {
+        if (showDisabledAchievementsMessage && !isIgnoredAchievement(event.getAdvancement().getKey().toString())) {
+            event.getPlayer().sendMessage(disabledAchievementsMessage);
+        }
     }
 }
+
